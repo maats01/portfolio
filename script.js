817223082738974a40957de5e3eb2256334a9355
Matheus <matheusp.sampaio@gmail.com>
2025-08-15 15:12:20 -0300

    Adicionar tema escuro e botão para alternar entre os temas

--- script.js
+++ script.js
@@ -19,3 +19,44 @@ document.addEventListener('DOMContentLoaded', function() {
     sr.reveal('.project-card', { interval: 150 });
 
 });
+
+// funcionalidade que altera o tema de cores do site
+
+const themeToggleBtn = document.getElementById('theme-toggle-btn');
+const body = document.body;
+const themeIcon = themeToggleBtn.querySelector('i');
+
+// tema salvo no local storage do usuário
+const savedTheme = localStorage.getItem('theme') || 'light';
+
+// função para aplicar os estilos corretos no HTML, de acordo com o tema
+function applyTheme(theme) {
+    if (theme === 'dark') {
+        body.classList.add('dark-mode');
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+        themeToggleBtn.title = "Alternar para tema claro";
+    } else {
+        body.classList.remove('dark-mode');
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+        themeToggleBtn.title = "Alternar para tema escuro";
+    }
+}
+
+// aplica o tema salvo do usuário ou aplica o tema padrão
+applyTheme(savedTheme);
+
+// evento de click no botão de alterar o tema
+themeToggleBtn.addEventListener('click', () => {
+    // verifica se o tema atual é escuro
+    const isDarkMode = body.classList.contains('dark-mode');
+    
+    // define o novo tema de acordo com o tema atual
+    const newTheme = isDarkMode ? 'light' : 'dark';
+    
+    applyTheme(newTheme);
+    
+    // salva o novo tema no localStorage
+    localStorage.setItem('theme', newTheme);
+});
